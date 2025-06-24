bc3cb3b1c408927805e61c170e4335d87f7e7c09
Soviet Cat <161828581+Soviet-Cat@users.noreply.github.com>
2025-06-25 08:31:09 +1000

    8:31 AM 25/6/2025

--- src/glb.cpp
+++
@@ -1,9 +0,0 @@
-#include "glb.hpp"
-
-const int glb::WINDOW_WIDTH = 1280;
-const int glb::WINDOW_HEIGHT = 720;
-const char* glb::WINDOW_TITLE = "CyberSwitch";
-
-SDL_Event* glb::event = nullptr;
-SDL_Window** glb::window = nullptr;
-SDL_GLContext* glb::context = nullptr;
